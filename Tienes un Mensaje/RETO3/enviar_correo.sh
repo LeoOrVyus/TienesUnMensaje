911ef538922a3b37076676a138f79443fac0a975
Leonardo Mtz <119015812+LeoOrVyus@users.noreply.github.com>
2023-04-23 14:30:15 -0600

    Delete enviar_correo.sh

--- Tienes un Mensaje/RETO3/enviar_correo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enviando correo electrónico..."
-
-while IFS=',' read -r col1 col2 col3 email
-do
-    echo "Enviando correo a $email..."
-    echo "Feliz Lunes compañeritos" | mutt -s "Feliz Lunes a Todos" -a "C:/Users/clase/OneDrive/Escritorio/NAO Evidencias/correos.csv" -- "$email"
-done < correos.csv
